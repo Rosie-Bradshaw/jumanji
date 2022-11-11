3a1f5bcfc4c0241e027bdbe3393d287ce7dc48a9
Rguzman77 <rguzman_7@hotmail.com>
2022-11-11 13:21:26 +0100

    Created see more button

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-
-
-const seeMore=getElementsByClassName("foldable")
+function seeMore() {
+    var x = document.getElementById("foldable");
+    var y = document.getElementById("buttonseemore")
+    if (x.style.display === "none") {x.style.display = "grid"  
+      y.innerHTML="Ver menos tematicas";
+    } else {
+      x.style.display = "none";
+      y.innerHTML="Ver más tematicas"
+    }
+ }
+  
